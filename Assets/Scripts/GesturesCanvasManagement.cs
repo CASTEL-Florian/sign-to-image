8203eb5ef55948b2bb85d84d13701f110c3c2b8f
arthur.audrain <arthur.audrain@ensiie.fr>
2022-11-21 21:08:51 +0100

    grosse modif  reco gest (save + del + list)

--- Assets/Scripts/GesturesCanvasManagement.cs
+++ Assets/Scripts/GesturesCanvasManagement.cs
@@ -10,10 +10,14 @@ public class GesturesCanvasManagement : MonoBehaviour
 
     public GameObject[] GestureFrames;
 
+    public TMP_InputField debugLog, debugLog2;
+
+    private int k = 0, j = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine(DelayRoutine(1));
+        StartCoroutine(DelayRoutine(1));
     }
 
     // Update is called once per frame
@@ -21,30 +25,36 @@ public class GesturesCanvasManagement : MonoBehaviour
     public IEnumerator DelayRoutine(float delay)
     {
         yield return new WaitForSeconds(delay);
-        UpdateCanvas();
+        UpdateCanvas(gestureDetection.gestures);
     }
 
-    public void UpdateCanvas()
+    public void UpdateCanvas(List<Gesture> gestureList)
     {
-        gestureList = gestureDetection.gestures;
+        j++;
         if (gestureList.Count != 0)
         {
+            k++;
             for (int i = 0; i < GestureFrames.Length; i++)
             {
-                GestureFrames[i].SetActive(false);
+                GestureFrames[i].SetActive(false);   
             }
-
+            
             for (int i = 0; i < gestureList.Count; i++)
             {
                 GestureFrames[i].SetActive(true);
                 GestureFrames[i].GetComponentInChildren<TextMeshProUGUI>().text = gestureList[i].name;
-                GetComponentInChildren<ButtonVR>().onRelease.AddListener(delegate { DeleteGesture(gestureList[i].name); });
+                GestureFrames[i].GetComponentInChildren<ButtonsGesturesCanvas>().buttonName = gestureList[i].name;
             }
-        } 
+            
+        }
+        debugLog.text = "j = " + j.ToString();
+        debugLog2.text = " k = " + k.ToString();
     }
 
     public void DeleteGesture(string name)
     {
+        gestureList = gestureDetection.gestures;
+
         for (int i = 0; i < gestureList.Count; i++)
         {
             if(gestureList[i].name == name)
@@ -53,9 +63,18 @@ public class GesturesCanvasManagement : MonoBehaviour
                 break;
             }
         }
+
+        // save the new gesture list 
         GestureList gestureClass = new GestureList();
         gestureClass.gestures = gestureDetection.gestures;
         gestureDetection.dataHandler.Save(gestureClass);
-        UpdateCanvas();
+
+        // update the canvas with the new gesture list
+        UpdateCanvas(gestureDetection.gestures);
+    }
+
+    public void PrintName(string name)
+    {
+        debugLog.text = name;
     }
 }
