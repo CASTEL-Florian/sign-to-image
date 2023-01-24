6447588d0d5a44e12dd76df31d92c0a49c1ff050
Hippolyte <hippolytedubois@yahoo.fr>
2023-01-24 16:41:17 +0000

    update script & prefab according

--- Assets/Scripts/GrimoireCarousel.cs
+++ Assets/Scripts/GrimoireCarousel.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using System.Collections.Generic;
+using System.Collections;
 
 public class GrimoireCarousel : MonoBehaviour
 {
-    [SerializeField] private Button nextButton;
-    [SerializeField] private Button previousButton;
+    public GestureDetection gestureDetection;
+    private List<Gesture> gestureList;
+
+    public GameObject rightHand;
+    public GameObject leftHand;
+
+    public List<GameObject> rightFingerBones;
+    public List<GameObject> leftFingerBones;
+    private Coroutine showRoutine;
+
+
+/*    [SerializeField] private Button nextButton;
+    [SerializeField] private Button previousButton;*/
     [SerializeField] private Image SignSpriteImageFirstPage;
     [SerializeField] private TextMeshProUGUI SignNameTextFirstPage;
     [SerializeField] private TextMeshProUGUI SignDescriptionTextFirstPage;
@@ -23,8 +35,8 @@ public class GrimoireCarousel : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        nextButton.onClick.AddListener(NextSign);
-        previousButton.onClick.AddListener(PreviousSign);
+/*        nextButton.onClick.AddListener(NextSign);
+        previousButton.onClick.AddListener(PreviousSign);*/
       
         // retourButon.onClick.AddListener(retour);
 
@@ -78,5 +90,54 @@ public class GrimoireCarousel : MonoBehaviour
 
 
        
+    }
+
+
+    public void showGesture(string name)
+    {
+        if (showRoutine != null)
+        {
+            rightHand.transform.position = new Vector3(0, 0, 0);
+            rightHand.transform.rotation = Quaternion.Euler(0, 0, 0);
+            leftHand.transform.position = new Vector3(0, 0, 0);
+            leftHand.transform.rotation = Quaternion.Euler(0, 0, 0);
+            leftHand.SetActive(false);
+            rightHand.SetActive(false);
+            StopCoroutine(showRoutine);
+        }
+        gestureList = gestureDetection.gestures;
+
+        for (int i = 0; i < gestureList.Count; i++)
+        {
+            if (gestureList[i].name == name)
+            {
+                leftHand.SetActive(true);
+                rightHand.SetActive(true);
+                for (int k = 0; k < gestureList[i].leftFingerDatas.Count; k++)
+                {
+                    rightFingerBones[k].transform.position = gestureList[i].rightFingerDatas[k];
+                    rightFingerBones[k].transform.rotation = gestureList[i].rightFingerRotations[k];
+                    leftFingerBones[k].transform.position = gestureList[i].leftFingerDatas[k];
+                    leftFingerBones[k].transform.rotation = gestureList[i].leftFingerRotations[k];
+                }
+                rightHand.transform.position = new Vector3(0.25f, 1.5f, 0.4f);
+                rightHand.transform.rotation = Quaternion.Euler(0, -90f, -90f);
+                leftHand.transform.position = new Vector3(-0.25f, 1.5f, 0.4f);
+                leftHand.transform.rotation = Quaternion.Euler(0, -90f, 90f);
+                showRoutine = StartCoroutine(DesactivateHands(10f));
+                break;
+            }
+        }
+    }
+
+    public IEnumerator DesactivateHands(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        rightHand.transform.position = new Vector3(0, 0, 0);
+        rightHand.transform.rotation = Quaternion.Euler(0, 0, 0);
+        leftHand.transform.position = new Vector3(0, 0, 0);
+        leftHand.transform.rotation = Quaternion.Euler(0, 0, 0);
+        leftHand.SetActive(false);
+        rightHand.SetActive(false);
     }
 }
