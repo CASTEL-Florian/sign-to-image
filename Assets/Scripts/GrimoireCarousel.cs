34dd0b54eda338efbc265fd9389a6e0438d53871
hippolyte.dubois <hippolyte.dubois@ensiie.fr>
2023-01-25 00:54:29 +0100

    debug

--- Assets/Scripts/GrimoireCarousel.cs
+++ Assets/Scripts/GrimoireCarousel.cs
@@ -146,9 +146,9 @@ public class GrimoireCarousel : MonoBehaviour
                     leftFingerBones[k].transform.position = gestureList[i].leftFingerDatas[k];
                     leftFingerBones[k].transform.rotation = gestureList[i].leftFingerRotations[k];
                 }
-                rightHand.transform.position = transform.position + new Vector3(0.25f, 0f, 0.4f) ;
+                rightHand.transform.position = transform.position + new Vector3(0.25f, 1.4f, 0.4f) ;
                 rightHand.transform.rotation = Quaternion.Euler(0, 90f, -90f);
-                leftHand.transform.position = transform.position + new Vector3(-0.25f, 0f, 0.4f);
+                leftHand.transform.position = transform.position + new Vector3(-0.25f, 1.4f, 0.4f);
                 leftHand.transform.rotation = Quaternion.Euler(0, 90f, 90f);
                 showRoutine = StartCoroutine(DesactivateHands(10f));
                 break;
@@ -177,20 +177,22 @@ public class GrimoireCarousel : MonoBehaviour
                 return g.name.Contains(name);
         }
         ).name;
-        SignDescriptionTextSecondPage.SetText("plip");
-
+        SignDescriptionTextSecondPage.SetText(trueName);
+        SignDescriptionTextFirstPage.SetText(name);
         showGesture(trueName);
     }
 
     //button call
-    public void EqName(Transform t)
+    public void EqName(TextMeshProUGUI txt)
     {
-        SignDescriptionTextSecondPage.SetText("plap");
-        string name = t.GetComponent<TextMeshPro>().text;
+        string name = txt.text;
         if (name != null)
         {
-            SignDescriptionTextSecondPage.SetText("plop");
             FindContaining(EnName[name]);
+        }
+        else
+        {
+            FindContaining(EnName["Marin"]);
         }
             
     }
