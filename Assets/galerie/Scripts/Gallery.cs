3945452a3730cc79a6ea2c0e47cd64fa61f58c6c
Nemmassi <ayoub.nemmassi@gmail.com>
2023-01-29 15:52:24 +0100

    gallery fixed

--- Assets/galerie/Scripts/Gallery.cs
+++ Assets/galerie/Scripts/Gallery.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Gallery : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int id;
      public int capacity;
      public Transform[] docks;
      public List<Frame> frames;
